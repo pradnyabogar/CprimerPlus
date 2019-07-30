25b4b64449f89e56d4e491519883209fa82e9155
pradnyabogar <pradnyabbogar@gmail.com>
2019-07-30 13:17:05 -0700

    Added more ways to get compiler warnings

--- Ch2_Introducing_C/Listing2_1_first_modified.c
+++ Ch2_Introducing_C/Listing2_1_first_modified.c
@@ -7,6 +7,17 @@ int main (void)					/* a simple program		*/
 	printf("computer.\n");
 	printf("Value of num stored in memory is %d which is garbage value.\n, num");
 	printf(" I dont't care if num = 1 \n");
-	printf("This is because compiler warning on line 8 of the code");
+								/* This is because compiler warning on line 8 of the code */
+	
+	static a;
+	printf("Value of static 'a' with no type is %d \n", a);
+								/* Compiler warning */
+		
+	static int b;
+	printf("Value of static int 'b' is %d \n", b);
+	
+	static c;
+	printf("Value of static 'c' with no type but with % f is %d \n", c);
+								/* Compiler warning. %f was recognised even with space */
 	return 0;
 }	
