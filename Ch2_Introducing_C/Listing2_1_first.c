da76ddcbb27d01adfd45ee8e44e1cecdc5234046
pradnyabogar <pradnyabbogar@gmail.com>
2019-07-30 07:20:15 -0700

    did not compiled. Compiled and executed.  # Try running the code with quotes before num and after num  # in the last printf statement and see different output

--- Ch2_Introducing_C/Listing2_1_first.c
+++ Ch2_Introducing_C/Listing2_1_first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main (void)					/* a simple program		*/
-(
+{
 	int num;					/* Define a variable called num */
 	num = 1;					/* assign a value to num */
-	printf("I am a simple");	/* use of the printf() function */
+	printf("I am a simple ");	/* use of the printf() function */
 	printf("computer.\n");
-	printf("My favourite number is %d because it is first.\n, num");
+	printf("My favourite number is %d because it is first.\n", num);
 	
 	return 0;
 }	
