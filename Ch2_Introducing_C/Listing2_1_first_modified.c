732eb9ec85cff87812014c2be91f93d4dc463849
pradnyabogar <pradnyabbogar@gmail.com>
2019-07-30 13:02:38 -0700

    Edited code to get garbage values despite assigning values to the var

---
+++ Ch2_Introducing_C/Listing2_1_first_modified.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)					/* a simple program		*/
+{
+	int num;					/* Define a variable called num */
+	num = 1;					/* assign a value to num */
+	printf("I am a simple ");	/* use of the printf() function */
+	printf("computer.\n");
+	printf("Value of num stored in memory is %d which is garbage value.\n, num");
+	printf(" I dont't care if num = 1 \n");
+	printf("This is because compiler warning on line 8 of the code");
+	return 0;
+}	
