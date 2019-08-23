7f4aed645ffd5596123750308ff2fc14a9077b6e
pradnyabogar <pradnyabbogar@gmail.com>
2019-08-22 22:40:59 -0700

    Chapter 4 Strlen function introduction

--- Ch4_Char_strings/L4_2_praise.c
+++ Ch4_Char_strings/L4_2_praise.c
@@ -6,9 +6,9 @@ int main(void)
 {
 	char name[40];
 	
-	printf("'What's your name?");
+	printf("'What's your name? ");
 	scanf("%s", name);
-	printf("'Hello %s. %s\n" name, PRAISE);
+	printf("Hello %s. %s\n", name, PRAISE);
 	
 	return 0;
 }
