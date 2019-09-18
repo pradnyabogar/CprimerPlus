72f383b7bd501b0ae318c315f0583bc015f5fd97
pradnyabogar <pradnyabbogar@gmail.com>
2019-09-17 21:28:17 -0700

    integer conversion in different format

---
+++ Ch4_Char_strings/L4_11_intconv.c
@@ -0,0 +1,18 @@
+/*Listing 4.11 intconv.c -- Some mismatched integer conversions */
+
+#include<stdio.h>
+# define PAGES 336
+# define WORDS 65618
+
+int main()
+{
+	short num = PAGES;
+	short mnum = -PAGES;
+	
+	printf("num as short and unsigned short: %hd %hu\n", num, num);
+	printf("-num as short and unsigned short: %hd %hu\n", mnum, mnum);
+	printf("num as int and char: %d %c\n", num, num);
+	printf("WORDS as int, short and char: %d %hd %c\n", WORDS, WORDS, WORDS);
+	
+	return 0;	
+}
