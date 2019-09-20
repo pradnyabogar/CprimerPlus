c7cf3f42be5559143040eba79cff527260270401
pradnyabogar <pradnyabbogar@gmail.com>
2019-09-19 19:32:11 -0700

    New ways to print long strings and float conv

---
+++ Ch4_Char_strings/L4_12_floatconv.c
@@ -0,0 +1,17 @@
+/*Listing 4.12 floatconv.c -- Some mismatched float conversions */
+
+#include<stdio.h>
+
+int main()
+{
+	float n1 = 3.0;
+	double n2 = 3.0;
+	long n3 = 2000000000;
+	long n4 = 1234567890;
+	
+	printf("%.1e %.1e %.1e %.1e\n", n1, n2, n3, n4);
+	printf("%ld %ld\n", n3, n4);
+	printf("%ld %ld %ld %ld\n", n1, n2, n3, n4);
+	
+	return 0;	
+}
