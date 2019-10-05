81be83dae0aa790f47f7ec20b4f6ad8e5372786b
pradnyabogar <pradnyabbogar@gmail.com>
2019-10-04 19:19:26 -0700

    Rules of predence through examples

---
+++ Ch5_Operators_Exp/L5_8_size.c
@@ -0,0 +1,14 @@
+/*5.8 sizeof.c -- use of sizeof */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int n =0;
+	size_t intsize;
+	
+	intsize = sizeof(int);
+	printf("n = %d, n has %zd bytes; all ints have %zd bytes.\n", n, sizeof n, intsize);
+
+	return 0;	
+}
