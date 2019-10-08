09f00a28ac7d0e52e863e81ceb52dd39e1e0c244
pradnyabogar <pradnyabbogar@gmail.com>
2019-10-07 20:46:28 -0700

    Increment and decrement ops post pefix examples

---
+++ Ch5_Operators_Exp/L5_11_prepost.c
@@ -0,0 +1,16 @@
+/*5.11 post_pre.c -- incrementing: prefix and postfix */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 1, b = 1;
+	int a_post, pre_b;
+
+	a_post=a++;
+	pre_b = ++b;
+	printf("a   a_post  b   pre_b\n");
+	printf("%1d, %5d %5d %5d\n", a, a_post,b,pre_b);
+	
+	return 0;
+}
