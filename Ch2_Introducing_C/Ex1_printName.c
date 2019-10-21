ccf0457a12ee95fe9c0925e92965228879279769
pradnyabogar <pradnyabbogar@gmail.com>
2019-10-21 13:07:34 -0700

    Changed 6.1 example and tested  * Changed folder name for convenience

--- Ch2_Introducing_C/Ex1_printName.c
+++ Ch2_Introducing_C/Ex1_printName.c
@@ -8,12 +8,17 @@ int main(void)
 	//fname = Gustav;
 	//lname = Mahler;
 	
-	printf("Type your first and last name:");
-	scanf("%s%s", fname, lname);
-	
+	//printf("Type your first and last name:");
+	//scanf("%s %s", fname, lname);
+	fname[0] = 'a';
+	fname[1] = 0;
+	lname[0] = 'b';
+	lname[1] = 0;
+	//you can use strlen to find if whitespace was considered
 	printf("%s %s\n", fname, lname);
-	printf("%s \n %s\n", fname, lname);
+	printf("%s \n%s\n", fname, lname);
 	printf("%s ", fname);
 	printf("%s\n",lname);
+
 	return 0;	
 }
