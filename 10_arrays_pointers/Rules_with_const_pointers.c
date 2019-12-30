d4c3514ef8e3e8a66d0acac8e7ffa64e8fb9f5fc
pradnyabogar <pradnyabbogar@gmail.com>
2019-12-30 03:07:02 -0800

    Array pointer have errors if declared and assgined outside main
     * hence moved it inside main to see what happens
     * Going to look up why this is the case

--- 10_arrays_pointers/Rules_with_const_pointers.c
+++ 10_arrays_pointers/Rules_with_const_pointers.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-double rates[5] = {88.99, 100.12, 59.45, 183, 340.5};
-const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
+//double rates[5] = {88.99, 100.12, 59.45, 183, 340.5};
+//double locked[4] = {0.08, 0.75, 0.0725, 0.07};
 //const double *pc = rates;
-const double * pc = locked;
-
+//double * pc;
+//const int a = 5;
+//pc = locked;
+//int * pc = a;
 
 void main(){
-    //return 0;
+    const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
+    const double * pc;
+    pc = locked;
+    printf("%p \n", pc);
 }
