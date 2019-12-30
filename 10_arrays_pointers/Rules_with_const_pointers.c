8a5c66c71ec9a1a4d5a2315a84b9808d33f34db3
pradnyabogar <pradnyabbogar@gmail.com>
2019-12-29 22:24:40 -0800

    Pointer to const assigned to non-const data

---
+++ 10_arrays_pointers/Rules_with_const_pointers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+double rates[5] = {88.99, 100.12, 59.45, 183, 340.5};
+const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
+//const double *pc = rates;
+const double *pc;
+pc = locked;
+
+void main(){
+    //return 0;
+}
