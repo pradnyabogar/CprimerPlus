025584c5c7af0023e26cbca8d75ecdd0b02cf1a4
pradnyabogar <pradnyabbogar@gmail.com>
2019-12-29 22:29:05 -0800

    Pointer to const asigned to a constant data

--- 10_arrays_pointers/Rules_with_const_pointers.c
+++ 10_arrays_pointers/Rules_with_const_pointers.c
@@ -2,8 +2,8 @@
 double rates[5] = {88.99, 100.12, 59.45, 183, 340.5};
 const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
 //const double *pc = rates;
-const double *pc;
-pc = locked;
+const double * pc = locked;
+
 
 void main(){
     //return 0;
