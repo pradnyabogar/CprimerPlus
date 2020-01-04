f6ea14a2aaa5331ad620e10706e60b6d2c2baeae
pradnyabogar <pradnyabbogar@gmail.com>
2020-01-03 18:51:06 -0800

    You can use cnst twice to create a pointer that can neither chnage where its pointing nor change the value to which it points

--- 10_arrays_pointers/Rules_with_const_pointers.c
+++ 10_arrays_pointers/Rules_with_const_pointers.c
@@ -8,8 +8,9 @@
 //int * pc = a;
 
 void main(){
-    const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
-    const double * pc;
-    pc = locked;
+    //const double locked[4] = {0.08, 0.75, 0.0725, 0.07};
+    double rates[5] = {88.99, 100.12, 59.45, 183, 340.5};
+    const double * const pc = rates;
+    //pc = locked;
     printf("%p \n", pc);
 }
